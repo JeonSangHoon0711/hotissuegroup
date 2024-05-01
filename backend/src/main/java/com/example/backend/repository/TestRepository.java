013a083fd90733a09969de32f0b0b051e463462e
JeonSangHoon0711 <121441565+JeonSangHoon0711@users.noreply.github.com>
2024-05-02 00:27:19 +0900

    Delete backend directory

--- backend/src/main/java/com/example/backend/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
